d9c20b1b3c7a7cf5e4cdf543a555e9fa67890862
Tzu-An Leong <78155146+leong86@users.noreply.github.com>
2023-04-18 11:16:01 -0700

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAYH8zlBLaiSKgvUiltdAweUwErU7mdtb0",
+  authDomain: "lighthalll2.firebaseapp.com",
+  databaseURL: "https://lighthalll2-default-rtdb.firebaseio.com",
+  projectId: "lighthalll2",
+  storageBucket: "lighthalll2.appspot.com",
+  messagingSenderId: "1082065299530",
+  appId: "1:1082065299530:web:6f7e7206403ce44cb1deb3"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
