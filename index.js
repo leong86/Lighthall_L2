959bb1da253cc665966614118a4748077d08e85c
Tzu-An Leong <78155146+leong86@users.noreply.github.com>
2023-04-21 16:15:39 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ document.getElementById("loginForm").addEventListener("submit",(event)=>{
 
 firebase.auth().onAuthStateChanged((user)=>{
     if(user){
-        location.replace("welcome.html")
+        location.replace("main.html")
     }
 })
 
-function login(){
-    const email = document.getElementById("email").value
-    const password = document.getElementById("password").value
-    firebase.auth().signInWithEmailAndPassword(email, password)
-    .catch((error)=>{
-        document.getElementById("error").innerHTML = error.message
-    })
-}
+// function login(){
+//     const email = document.getElementById("email").value
+//     const password = document.getElementById("password").value
+//     firebase.auth().signInWithEmailAndPassword(email, password)
+//     .catch((error)=>{
+//         document.getElementById("error").innerHTML = error.message
+//     })
+// }
 
 function login() {
     const email = document.getElementById("email").value;
--- task.js
+++ task.js
@@ -1,72 +1,51 @@
 
+function addTask() {
+  const title = document.getElementById("title").value;
+  const start = document.getElementById("start").value;
+  const due = document.getElementById("due").value;
+  const status = document.getElementById("status").value;
+  const userId = firebase.auth().currentUser.uid;
+  const taskRef = database.ref("users/" + userId + "/tasks" + title).push();
+  // console.log(userId);
+  taskRef.set({
+    title: title,
+    start: start,
+    due: due,
+    status: status,
+    userId: userId
+  }).then(() => {
+    updateTable();
+    console.log("Create successfully ");
+  }).catch(error => {
+    console.error("Error adding task: ");
+  });
+}
 
-  function addTask() {
-    const title = document.getElementById("title").value;
-    const start = document.getElementById("start").value;
-    const due = document.getElementById("due").value;
-    const status = document.getElementById("status").value;
-    const userId = firebase.auth().currentUser.uid;
-    const taskRef = database.ref("users/" + auth.currentUser.uid + "/tasks" + title).push();
-
-    taskRef.set({
-      title: title,
-      start: start,
-      due: due,
-      status: status,
-      userId: userId
-    }).then(() => {
-      // updateTable();
-      console.log("Create successfully ");
-    }).catch(error => {
-      console.error("Error adding task: ");
-    });
-  }
-  
-  
-
-  function deleteTask() {
-    const taskId = document.getElementById("delete").value;
-    console.log(taskId);
-    const taskRef = database.ref("users/" + auth.currentUser.uid + "/tasks" + taskId);
-    taskRef.remove().then(() => {
-      displayTasks();
-      // updateTable();
-      console.log("Task deleted successfully!");
-    }).catch(error => {
-      console.error("Error deleting task: ", error);
-    });
-  }
-  
-
-  function displayTasks() {
-    const taskList = document.getElementById("taskList");
-    const currentTasks = document.getElementById("currentTasks");
-    const userId = firebase.auth().currentUser.uid;
-    const taskRef = database.ref("users/" + userId + "/tasks");
-    taskRef.once("value", snapshot => {
-      taskList.innerHTML = "";
-      currentTasks.innerHTML = "";
-      snapshot.forEach(childSnapshot => {
-        const task = childSnapshot.val();
-        const taskId = childSnapshot.key;
-        const li = document.createElement("li");
-        li.appendChild(document.createTextNode(task.title + " - " + task.status));
-        taskList.appendChild(li);
-        if (task.status !== "Completed") {
-          const currentTask = document.createElement("p");
-          currentTask.appendChild(document.createTextNode(task.title));
-          currentTasks.appendChild(currentTask);
-        }
-      });
+function deleteTask() {
+  const taskId = document.getElementById("delete").value;
+  console.log(taskId);
+  const taskRef = database.ref("users/" + auth.currentUser.uid + "/tasks" + taskId);
+  taskRef.remove().then(() => {
+    updateTable();
+    console.log("Task deleted successfully!");
+  }).catch(error => {
+    console.error("Error deleting task: ", error);
   });
 }
 
 function updateTable() {
+  
   const taskList = document.getElementById("taskList");
   taskList.innerHTML = "";
-  const tasksRef = database.ref("users/" +  auth.currentUser.uid );
+  const userId = firebase.auth().currentUser.uid;
+  console.log(userId);
+  if (!userId) {
+    console.log("User not authenticated");
+    return;
+  }
 
-  tasksRef.once("value", function(snapshot) {
+  var dbRef = firebase.database().ref();
+  dbRef.child("users").child(userId).on("value", function(snapshot){
     snapshot.forEach(function(childSnapshot) {
       const task = childSnapshot.val();
       const taskId = childSnapshot.key;
@@ -75,4 +54,6 @@ function updateTable() {
     });
   });
 }
+
+
 window.onload = updateTable;
